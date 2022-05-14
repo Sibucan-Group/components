2f7bb980e50031f4e26073c2a7bb4237c8a55bf7
Josué Cortina <joshuacs.main@gmail.com>
2022-05-14 17:12:55 -0400

    feat: Add palette colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
   },
   plugins: [require('daisyui')],
   daisyui: {
-    prefix: 'dui-',
+    themes: [
+      {
+        light: {
+          'primary': '#12BBD1',
+          'primary-content': '#FFFFFF',
+          'secondary': '#EEBD25',
+          'secondary-content': '#FFFFFF',
+          'accent': '#34D399',
+          'accent-content': '#FFFFFF',
+          'neutral': '#19242E',
+          'neutral-content': '#FFFFFF',
+          'base-100': '#F7F7F7',
+          'info': '#71A3F4',
+          'success': '#1AA253',
+          'warning': '#F9D648',
+          'error': '#EC2B34',
+        },
+      },
+    ],
   },
 }
