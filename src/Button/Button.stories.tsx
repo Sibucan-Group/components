2615928c504dc33294ae83df1eedc4e63a4d46f8
Josué Cortina <joshuacs.main@gmail.com>
2022-05-14 23:56:12 -0400

    test: Add `Button` stories

---
+++ src/Button/Button.stories.tsx
@@ -0,0 +1,54 @@
+import { Story, Meta } from '@storybook/react'
+
+import { Button, ButtonProps } from './Button'
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta
+
+const Template: Story<ButtonProps> = args => <Button {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  color: 'primary',
+  children: 'Primary',
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  color: 'secondary',
+  children: 'Secondary',
+}
+
+export const Contained = Template.bind({})
+Contained.args = {
+  variant: 'contained',
+  children: 'Contained',
+}
+
+export const Gradient = Template.bind({})
+Gradient.args = {
+  variant: 'gradient',
+  color: 'primary',
+  children: 'Gradient',
+}
+
+export const Large = Template.bind({})
+Large.args = {
+  size: 'large',
+  children: 'Large',
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  size: 'small',
+  children: 'Small',
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  variant: 'contained',
+  disabled: true,
+  children: 'Disabled',
+}
