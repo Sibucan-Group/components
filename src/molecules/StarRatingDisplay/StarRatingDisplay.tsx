662de68066aaa02d2031fbab4c924cdcf77ebfa3
rizzrark <razzilsp@gmail.com>
2022-08-26 10:21:56 -0400

    feat: Add `StarRatingDisplay` component

---
+++ src/molecules/StarRatingDisplay/StarRatingDisplay.tsx
@@ -0,0 +1,38 @@
+import { StarIcon as StarFilledIcon } from '@heroicons/react/solid'
+import { StarIcon as StarEmptyIcon } from '@heroicons/react/outline'
+
+import { cnb } from 'cnbuilder'
+
+const stars = [1, 2, 3, 4, 5]
+
+const starSize = {
+  normal: 'w-4',
+  medium: 'w-6',
+  big: 'w-8',
+}
+
+export type StarRatingProps = {
+  rating: number
+  size: 'normal' | 'medium' | 'big'
+}
+
+export const StarRating = ({ rating, size }: StarRatingProps) => {
+  console.log('size', starSize[size])
+  return (
+    <div className='flex'>
+      {stars.map(i =>
+        i <= rating ? (
+          <StarFilledIcon
+            key={i}
+            className={cnb(starSize[size], 'text-yellow-400')}
+          />
+        ) : (
+          <StarEmptyIcon
+            key={i}
+            className={cnb(starSize[size], 'text-yellow-400')}
+          />
+        )
+      )}
+    </div>
+  )
+}
